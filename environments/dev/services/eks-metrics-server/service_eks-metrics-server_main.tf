9552493e49c0cb5354cdd99c1e03f62eaa21b24d
cloudnativesystem <cloud.native.system@gmail.com>
2022-07-07 19:52:15 -0400

    changing namespace

--- environments/dev/services/eks-metrics-server/service_eks-metrics-server_main.tf
+++ environments/dev/services/eks-metrics-server/service_eks-metrics-server_main.tf
@@ -1,5 +1,6 @@
 resource "helm_release" "metrics-server" {
   name       = "metrics-server"
+  namespace  = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "metrics-server"
   version    = "0.6.1"
