54bf277c650024148947879dd645858d45e80e18
cloudnativesystem <cloud.native.system@gmail.com>
2022-07-08 07:18:32 -0400

    removing helm chart

--- environments/dev/services/eks-metrics-server/service_eks-metrics-server_main.tf
+++ environments/dev/services/eks-metrics-server/service_eks-metrics-server_main.tf
@@ -1,10 +1,10 @@
-
+/*
 resource "helm_release" "metrics-server" {
   name       = "metrics-server"
   namespace  = "kube-system"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "metrics-server"
-}
+}*/
 
 
 
