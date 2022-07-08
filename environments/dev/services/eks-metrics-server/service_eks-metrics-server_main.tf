a52bdddbee4514e42efe8e6691f989f485b63be6
cloudnativesystem <cloud.native.system@gmail.com>
2022-07-07 20:14:54 -0400

    updating chart name

--- environments/dev/services/eks-metrics-server/service_eks-metrics-server_main.tf
+++ environments/dev/services/eks-metrics-server/service_eks-metrics-server_main.tf
@@ -2,6 +2,8 @@ resource "helm_release" "metrics-server" {
   name       = "metrics-server"
   namespace  = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "bitnami/metrics-server"
+  chart      = "metrics-server"
 }
+
+
 
